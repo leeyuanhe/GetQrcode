76725a66d839669be4c51e3e586dfb212dd7cced
heliy <2364849426@qq.com>
2019-08-24 23:34:46 +0800

    优化

--- src/getcode.py
+++ src/getcode.py
@@ -1,15 +1,12 @@
 import base64
-import subprocess
-import re
-from sys import platform
 from apscheduler.schedulers.blocking import BlockingScheduler
+from mping import mping
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from wxpy import *
-from time import sleep
 
 # 初始化机器人，扫码登陆
 bot = Bot(console_qr=False, cache_path=True)
@@ -32,52 +29,41 @@ def collect_fastest_ip():
         # options.add_argument('--headless')
         # options.add_argument('--disable-gpu')
         driver = webdriver.Chrome(options=options)
-        driver.get("https://ss-ss.ooo/")
+        driver.get("https://free-ss.site")
         wait = WebDriverWait(driver, 20, 0.2)
         # 循环遍历tr
         tr_num = 1
-        ip_list = []
+        orgip_list = []
         ip_map = {}
         url_map = {}
-        sleep(15)
-        while tr_num < 36:
-            tables = wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'table')))
-            print(tables[1].get_attribute("id"))
-            global div_id
-            div_id = tables[1].get_attribute("id")
-            ip = wait.until(
-                EC.presence_of_element_located((By.XPATH, '//*[@id="'+div_id+'"]/tbody/tr[' + str(tr_num) + ']/td['
-                                                                                                            '2]'))).text
+        tables = wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'table')))
+        print(tables[1].get_attribute("id"))
+        global div_id
+        div_id = tables[1].get_attribute("id")
+        tbody = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="'+div_id+'"]/tbody')))
+        elements = tbody.find_elements(By.TAG_NAME, "tr")
+        for row in elements:
+            ip = row.find_element_by_xpath(".//td[2]").text
+            orgip_list.append(ip)
+            ip_map[ip]=tr_num
             tr_num += 1
-            print(ip)
-            if platform == "linux" or platform == "darwin":
-                out = subprocess.Popen(['ping', '-c', '5', ip], stdout=subprocess.PIPE)
-                decode = out.stdout.read().decode()
-                index = decode.index('/')
-                print(decode[index + 1:index + 4] + ':' + decode[index + 26:index + 33])
-                avg_time = float(decode[index + 26:index + 33])
-            else:
-                out = subprocess.Popen(['ping', '-n', '5', ip], stdout=subprocess.PIPE)
-                decode = out.stdout.read().decode("gbk")
-                avg_str = re.search("平均 = (\d+)", str(decode)).group()
-                avg_time = float(avg_str[avg_str.index("=")+1:len(avg_str)].strip())
-
-            ip_list.append(avg_time)
-            get_png(wait, driver, ip_map, avg_time,url_map)
+        resultList = mping(orgip_list)
+        fastest_ip = resultList[0][0]
+        min_avg = str(resultList[0][1].avg)
+        print(fastest_ip+"======"+min_avg)
+        tr_num = ip_map[fastest_ip]
+        get_png(wait, driver, ip_map, min_avg,url_map,str(tr_num))
     except TimeoutException:
         print('===超时警告')
         pass
     finally:
         driver.close()
-        if len(ip_list) == 0:
+        if len(orgip_list) == 0:
             print('未获取到页面元素')
             return
-        print('最快的响应时间:' + str(min(ip_list)))
-        print('最慢的响应时间:' + str(max(ip_list)))
-        print(ip_map.keys())
-        print(str(min(ip_list)))
+        print("最快响应ip："+fastest_ip)
         # 将二维码发送给接收人
-        send_qrcode(receiver, ip_map[min(ip_list)], url_map[min(ip_list)], str(min(ip_list)))
+        send_qrcode(receiver, ip_map[min_avg], url_map[min_avg], min_avg)
 
 
 
@@ -86,9 +72,9 @@ def collect_fastest_ip():
 """
 
 
-def get_png(wait, driver, ip_map, avg_time,url_map):
+def get_png(wait, driver, ip_map, avg_time,url_map,tr_num):
     button = wait.until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="'+div_id+'"]/tbody/tr[1]/td[8]/i')))
+        EC.presence_of_element_located((By.XPATH, '//*[@id="'+div_id+'"]/tbody/tr['+tr_num+']/td[8]/i')))
     button.click()
     canvas = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="qrcode"]/canvas')))
     href_element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'URI')))
@@ -123,7 +109,7 @@ def send_qrcode(receiver, png, url, avgTime):
 if __name__ == '__main__':
     try:
         scheduler = BlockingScheduler()
-        scheduler.add_job(collect_fastest_ip, 'interval', seconds=300)
+        scheduler.add_job(collect_fastest_ip, 'interval', seconds=500)
         scheduler.start()
     except (KeyboardInterrupt, SystemExit, SystemError):
         print('===出错了')
